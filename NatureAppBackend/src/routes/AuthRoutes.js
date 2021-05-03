7698198d633e00b886671846a472a3001f89f987
Kysadin <filipe.miguel.mota2000@outlook.com>
2021-05-03 18:41:33 +0100

    Criação do user

--- NatureAppBackend/src/routes/AuthRoutes.js
+++ NatureAppBackend/src/routes/AuthRoutes.js
@@ -3,12 +3,29 @@ const db = require("../ConnectionDb");
 
 const router = express.Router();
 
-router.get("/signup", async (req, res) => {
-    //const {email, password} = req.body;
-    const sql = "SELECT * FROM users"
-    const doc = await db.query(sql);
-    res.json(doc);
+router.get("/signup", async (req, res) => { //get useres
+    const query = "SELECT * FROM users"
+    const result = await db.query(query);
+    res.json(result);
     
+});
+
+router.post("/signup", async (req, res) => { // create a user
+    const {email, password} = req.body;
+    const getCurrentDate = () => {
+        var date = new Date().getDate();
+        var month = new Date().getMonth() + 1;
+        var year = new Date().getFullYear();
+
+        return date + '-' + month + '-' + year;
+    };
+    try {
+        const query = `INSERT INTO users (user_email, user_name, user_password, date) VALUES ('${email}', 'TheGbossTV', '${password}', '${getCurrentDate()}')`
+        const result = await db.query(query);
+        res.send("User Created");
+    }catch(err){
+        return res.status(422).send("Email ou username já existente");
+    }
 });
 
 module.exports = router;
