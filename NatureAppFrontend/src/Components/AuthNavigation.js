32960d8f9cab097f5a27959884fd05af4725f574
Kysadin <filipe.miguel.mota2000@outlook.com>
2021-05-24 15:51:18 +0100

    Butão de registo e login retirado do componente

--- NatureAppFrontend/src/Components/AuthNavigation.js
+++ NatureAppFrontend/src/Components/AuthNavigation.js
@@ -2,12 +2,9 @@ import React from 'react';
 import { Text, View, StyleSheet, TouchableOpacity} from "react-native";
 import {withNavigation} from "react-navigation";
 
-const AuthNavigation = function({navigation, buttonTitle, textLink, navigationText}) {
+const AuthNavigation = function({navigation, textLink, navigationText}) {
     return(
         <View>
-            <TouchableOpacity style={styles.Button} onPress={() => console.log("PLS DONT KILL ME!")}>
-                <Text style={styles.Text}>{buttonTitle}</Text>
-            </TouchableOpacity>
             <TouchableOpacity onPress = {() => navigation.navigate(navigationText)}>
                 <Text style={styles.linkText}>{textLink}</Text>
             </TouchableOpacity>
@@ -16,24 +13,6 @@ const AuthNavigation = function({navigation, buttonTitle, textLink, navigationTe
 }
 
 const styles = StyleSheet.create({
-    Button: {
-        backgroundColor: "#011936",
-        borderRadius: 15,
-        paddingHorizontal: 15,
-        borderWidth: 0,
-        marginTop: 20,
-        elevation: 9,
-        height: 40,
-        width: 135,
-        alignItems: "center",
-        justifyContent: "center",
-        alignSelf: "center"
-    },
-    Text: {
-        color: "white",
-        fontWeight: "bold",
-        fontSize: 15
-    },
     linkText: {
         color: "#3380FF",
         marginTop: 15,
