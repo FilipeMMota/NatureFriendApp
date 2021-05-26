f50531f38cfe3325e756330a11405654f0160663
Kysadin <filipe.miguel.mota2000@outlook.com>
2021-05-26 18:14:21 +0100

    adição do error message

--- NatureAppFrontend/src/Screens/RegisterScreen.js
+++ NatureAppFrontend/src/Screens/RegisterScreen.js
@@ -1,18 +1,24 @@
-import React , {useState, useContext} from "react";
-import { Text, View, StyleSheet, TouchableOpacity, TextInput, Image } from "react-native";
+import React, {useState, useContext, useEffect} from "react";
+import { Text, View, StyleSheet, TouchableOpacity, TextInput, Image, useWindowDimensions } from "react-native";
+import {NavigationEvents} from "react-navigation";
 import AuthNavigation from "../Components/AuthNavigation";
 import {Context as AuthContext} from "../Context/AuthContext";
 
 const RegisterScreen = function ({ navigation }) {
-    const {state, signup} = useContext(AuthContext);
+
+    const {state, signup, clearErrorMessage, checkAuthentication} = useContext(AuthContext);
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [retypedPassword, setRetypedPassword] = useState("");
 
+    useEffect(() => {
+        checkAuthentication();
+    }, []);
 
     return (
         <View style={styles.Content}>
+            <NavigationEvents onWillFocus={clearErrorMessage} onWillBlur={clearErrorMessage}/>
             <Image style={styles.icon} source={require('../../assets/IconNature.png')} />
             <Text style={styles.title}>Nature Friend</Text>
 
@@ -21,13 +27,15 @@ const RegisterScreen = function ({ navigation }) {
             <TextInput secureTextEntry style={styles.labels} placeholder="Password" value={password} onChangeText={setPassword} autoCapitalize="none" autoCorrect={false}/>
             <TextInput secureTextEntry style={styles.labels} placeholder="Confirm Password" value={retypedPassword} onChangeText={setRetypedPassword} autoCapitalize="none" autoCorrect={false}/>
 
-            <TouchableOpacity style={styles.Button} onPress={() => signup({email, username, password})}>
+            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
+
+            <TouchableOpacity style={styles.Button} onPress={() => signup({email, username, password, retypedPassword})}>
                 <Text style={styles.Text}>Register</Text>
             </TouchableOpacity>
 
             <AuthNavigation 
                 textLink="Already have an Account?"
-                navigationText="Login" //CHANGE THIS TO "Login" WHEN DONE
+                navigationText="Login"
             />
 
             <TouchableOpacity onPress = {() => navigation.navigate("Posts")}>
@@ -88,7 +96,7 @@ const styles = StyleSheet.create(
             fontWeight: "bold",
             fontSize: 15
         },
-        WarningText: {
+        errorMessage: {
             color: "#E32A02",
             marginTop: 10,
             fontSize: 15,
