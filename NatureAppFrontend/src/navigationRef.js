988d268a92f12a09f62ef9bcdb46b2009f27241b
Kysadin <filipe.miguel.mota2000@outlook.com>
2021-05-26 18:13:03 +0100

    Criação do ficheiro que deixa navegar entre screens

---
+++ NatureAppFrontend/src/navigationRef.js
@@ -0,0 +1,14 @@
+//Este ficheiro é usado para fazer navegação para ficheiros que não sejam componentes do react
+import {NavigationActions} from "react-navigation";
+
+let navigator; //Esta variavél contém acesso à navegação entre screens. 
+
+export const setNavigator = (nav) => {// esta função serve para ir buscar o acesso às propriedades de navegação
+    navigator = nav; 
+};
+
+export const navigate = (routeName, params) => { // função que faz navegar entre screens
+    navigator.dispatch(
+        NavigationActions.navigate({ routeName, params})
+    );
+};
