c3b37c6178992e8c763d59b6ab2b50c233d64c5b
Kysadin <filipe.miguel.mota2000@outlook.com>
2021-06-02 01:22:34 +0100

    Adição da obtenção da localização do utilizador

--- NatureAppFrontend/src/Screens/MapScreen.js
+++ NatureAppFrontend/src/Screens/MapScreen.js
@@ -1,17 +1,70 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, {useState, useEffect} from "react";
+import { StyleSheet, View, Alert} from "react-native";
+import {Accuracy, requestForegroundPermissionsAsync, getCurrentPositionAsync} from "expo-location";
 import MapView from "react-native-maps";
 
 const MapScreen = () => {
-    return <MapView 
-        style={styles.map}
-        mapType= "hybrid"
-    />
+
+    const [accessGranted, setAccessGranted] = useState(true)
+    const [latitude, setLatitude] = useState(10)
+    const [longitude, setLongitude] = useState(10)
+    const startWatching = async () => {
+        try{
+            const {granted} = await requestForegroundPermissionsAsync();
+            if(!granted){
+                setAccessGranted(false)
+            }
+            setAccessGranted(true);
+            const location = await getCurrentPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+            })
+            setLatitude(location.coords.latitude)
+            setLongitude(location.coords.longitude)
+            
+        }catch(e){
+        }
+    };
+
+    const AlertForLocation = () =>
+    Alert.alert(
+      "Need location premission",
+      "This application needs the permission to use your location, Please enable the location",
+      [
+        { text: "Ok", onPress: () => startWatching() }
+      ],
+      { cancelable: false }
+    )
+
+    useEffect(() => {
+        startWatching();
+    }, []);
+
+    if(!accessGranted){
+        AlertForLocation();
+        return (<View></View>)
+    }
+
+    return (
+    <View style={{flex: 1}}>
+        <MapView 
+            style={styles.map}
+            mapType= "hybrid"
+            region={{latitude, longitude, latitudeDelta: 0.0900, longitudeDelta: 0.0900}}
+        />
+    </View>
+    );
 };
 
 const styles = StyleSheet.create({
     map: {
         flex: 1
+    },
+    errorText: {
+        fontSize: 33,
+        paddingTop: 60
+    },
+    button: {
+        marginTop: 100
     }
 });
 
