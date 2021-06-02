1c1834380437b625b22e5104b8b9da7357c885f0
Kysadin <filipe.miguel.mota2000@outlook.com>
2021-06-02 01:19:38 +0100

    Importação das rotas dos posts

--- NatureAppBackend/src/index.js
+++ NatureAppBackend/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');// Biblioteca de um token de segurança
 const authRoutes = require("./routes/AuthRoutes");
 const requireAuth = require("./middleware/requireAuth");
+const postsRoutes = require("./routes/PostsRoutes");
 
 const app = express();
 const port = 5000 // Porta onde o servidor está a correr
 
 app.use(express.json());
 app.use(authRoutes);
+app.use(postsRoutes);
 
 app.get("/", requireAuth, (req, res) => { // Rota default
     res.send(`Your email is ${req.user.user_email} your username is ${req.user.user_name}, and your account was created on ${req.user.date}`);
