a89bb3cd1058fde7a8a4e5571d0238da1399940d
Kysadin <filipe.miguel.mota2000@outlook.com>
2021-06-04 03:20:53 +0100

    camera screen

--- NatureAppFrontend/src/Screens/CameraScreen.js
+++ NatureAppFrontend/src/Screens/CameraScreen.js
@@ -1,10 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import { Text, StyleSheet, View} from 'react-native';
+import { Text, StyleSheet, View, TouchableOpacity} from 'react-native';
 import {Camera} from 'expo-camera';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {NavigationEvents} from "react-navigation";
 
-const CameraScreen = function() {
+
+const CameraScreen = function({navigation}) {
     const [type, setType] = useState(Camera.Constants.Type.back);
     const [hasPermission, setHaspermission] = useState(null);
+    const [isFocused, setIsFocused] = useState(true);
 
     useEffect( () => {
         (async () => {
@@ -24,10 +28,27 @@ const CameraScreen = function() {
 
     return (
         <View>
-            <Camera
+            <NavigationEvents onWillFocus={() => setIsFocused(navigation.isFocused())} onWillBlur={() => setIsFocused(navigation.isFocused())}/>
+            {isFocused && 
+            (<Camera
             type={type}
-            style={{flex:1}}
-            />       
+            style={styles.camera}
+            >
+                <View style={{flex:1, backgroundColor: 'transparent'}}>
+                    <TouchableOpacity
+                        style={styles.flipCamera}
+                        onPress={ () =>{
+                            setType(
+                                type === Camera.Constants.Type.back
+                                ? Camera.Constants.Type.front
+                                : Camera.Constants.Type.back
+                            );
+                        }}
+                    > 
+                    </TouchableOpacity>
+                </View>
+
+            </Camera>)}         
         </View>
     );
     }
