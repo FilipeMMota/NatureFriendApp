9c3c6e64b9b88fab9141cc232be3a36f910f4b30
Kysadin <filipe.miguel.mota2000@outlook.com>
2021-07-03 17:40:37 +0100

    Incluir as user routes e incluir as pastas para as fotos

--- NatureAppBackend/src/index.js
+++ NatureAppBackend/src/index.js
@@ -2,13 +2,19 @@ const express = require("express"); // Biblioteca de um token de segurança
 const authRoutes = require("./routes/AuthRoutes");
 const requireAuth = require("./middleware/requireAuth");
 const postsRoutes = require("./routes/PostsRoutes");
+const userRoutes = require("./routes/UserRoutes");
+const UserImages = require("path").join(__dirname, "/UserImages");
+const PostImages = require("path").join(__dirname, "/PostImages");
 
 const app = express();
 const port = 5000; // Porta onde o servidor está a correr
 
 app.use(express.json());
+app.use(express.static(UserImages));
+app.use(express.static(PostImages));
 app.use(authRoutes);
 app.use(postsRoutes);
+app.use(userRoutes);
 
 app.get("/", requireAuth, (req, res) => {
   // Rota default
