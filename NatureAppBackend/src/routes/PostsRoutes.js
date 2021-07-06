571d8a0489a29f8ff70edbf0af30d685faf7abd5
Kysadin <filipe.miguel.mota2000@outlook.com>
2021-07-06 18:35:07 +0100

    Mudança na obtenção de todos os posts e adição da opção de delete

--- NatureAppBackend/src/routes/PostsRoutes.js
+++ NatureAppBackend/src/routes/PostsRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express"); // Biblioteca para o desenvolvimento de APIs
 const db = require("../ConnectionDb");
-const requireAuth = require("../middleware/requireAuth");
-const multer = require("multer");
+const requireAuth = require("../middleware/requireAuth"); // Middleware
+const multer = require("multer"); // Usado para transferencia de ficheiros.
+const fs = require("fs-extra"); // usado para trabalar com files
+const path = require("path"); // usado para trabalhar com caminhos
 const router = express.Router();
+
+const PostImagesPath = path.join(__dirname, "../PostImages"); //Indicamos o path até à pasta onde ficam guardads as fotos
 
 router.use(express.json());
 
 const UploadPostImage = multer.diskStorage({
+  // Aqui é onde é definido onde é que o ficheiro é guardado e com que nome vai ficar guardado
   destination(req, file, callback) {
     callback(null, "./src/PostImages");
   },
@@ -17,29 +22,38 @@ const UploadPostImage = multer.diskStorage({
 
 const upload = multer({ storage: UploadPostImage });
 
-router.get("/posts", requireAuth, async (req, res) => {
-  //Rota para a obtenção dos users
+router.get("/userPosts", requireAuth, async (req, res) => {
+  //Rota para a obtenção das publicações dos utilizadores
   try {
     const { user_id: id } = req.user;
-    const query = `SELECT * FROM posts WHERE user_id = ${id}`; // Crição de um novo User
+    const query = `SELECT * FROM posts WHERE user_id = '${id}'`; // Obtenção das publicações de um certo id de um utilizador
     const result = await db.query(query);
 
     res.status(200).send(result.rows);
   } catch (err) {
-    return res.status(422).send("Algo correu mal"); // Caso algo no processo de criação de um usuário ou na criação do token correr mal é mandada uma mensagem de erro
+    return res.status(422).send("Algo correu mal");
+  }
+});
+
+router.get("/allPosts", requireAuth, async (req, res) => {
+  //Rota para a obtenção de todas as publicações
+  try {
+    const query = `SELECT * FROM posts`; // Obtenção de todas as publicações feitas pelos utilizadores
+    const result = await db.query(query);
+
+    res.status(200).send(result.rows);
+  } catch (err) {
+    return res.status(422).send("Algo correu mal");
   }
 });
 
 router.post(
   "/posts",
   requireAuth,
-  upload.single("PostPicture"),
+  upload.single("PostPicture"), // Aqui indica o "id" da foto que nos queremos receber nesta função
   async (req, res) => {
     try {
       const { title, description, latitude, longitude } = req.body;
-      console.log("file", req.file);
-      console.log("body", req.body);
-      console.log("title: ", title);
       const { user_id: id } = req.user;
 
       const getCurrentDate = () => {
@@ -62,5 +76,29 @@ router.post(
     }
   }
 );
+
+router.delete("/posts", requireAuth, async (req, res) => {
+  //Rota para a remover uma publicação
+  try {
+    const { id } = req.body;
+
+    const query = `SELECT post_img FROM posts WHERE post_id = ${id}`; // remoção de uma publicação de um utilizador
+    const result = await db.query(query);
+
+    let files = fs.readdirSync(PostImagesPath);
+    if (files.includes(result.rows[0].post_img)) {
+      fs.unlinkSync(
+        path.join(__dirname, "../PostImages") + "\\" + result.rows[0].post_img
+      );
+    }
+
+    const queryDelete = `DELETE FROM posts WHERE post_id = ${id}`; // remoção de uma publicação de um utilizador
+    await db.query(queryDelete);
+
+    res.status(200).send({ message: "Post Deleted" });
+  } catch (err) {
+    return console.log(err); //res.status(422).send("Algo correu mal");
+  }
+});
 
 module.exports = router;
