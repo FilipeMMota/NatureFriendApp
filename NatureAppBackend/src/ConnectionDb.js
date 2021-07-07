624a3057030eda8534a413893cbcd07f3006402e
Kysadin <filipe.miguel.mota2000@outlook.com>
2021-07-07 11:57:10 +0100

    database

--- NatureAppBackend/src/ConnectionDb.js
+++ NatureAppBackend/src/ConnectionDb.js
@@ -1,28 +1,15 @@
 const { Client } = require("pg"); // importação da biblioteca do postgreSQL
 require("dotenv").config(); // Biblioteca para esconder informações importantes no código
 
-if (process.env.PRODUCTION == true) {
-  const db = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+const db = new Client({
+  // onde é colocado todos os dados necessários para a conexão à base de dados
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
-  db.connect();
+db.connect(console.log("Connectado à base de dados")); // Coneção à base de dados
 
-  module.exports = db;
-} else {
-  const db = new Client({
-    // onde é colocado todos os dados necessários para a conexão à base de dados
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-  });
-
-  db.connect(console.log("Connectado à base de dados")); // Coneção à base de dados
-
-  module.exports = db; // Exportação da conexão à base de dados para fazer querys
-}
+module.exports = db; // Exportação da conexão à base de dados para fazer querys
